1402fbde9efafa0abd4eaaf9cef759fcd90c6c37
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-23 13:17:02 +0700

    1234

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
 import { Button } from "@mui/material";
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Page1 from "./components/Page1";
+import Page2 from "./components/Page2";
 
 type Props = {};
 
 export default function App({}: Props) {
   return (
     <div>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+      <Routes>
+        <Route path="page1" element={<Page1 />} />
+        <Route path="page2" element={<Page2 />} />
+      </Routes>
     </div>
   );
 }
