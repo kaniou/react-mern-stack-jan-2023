5cc572e3046d78ed906064f1be18f717d65cb081
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-23 11:01:36 +0700

    1234

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,21 @@ type Props = {};
 export default function App({}: Props) {
   // Typescript
   const tmp1 = "lek";
+  const tmp2: string = "codemobiles";
+  const tmp3: number = 2134;
 
   // JSX(TSX)
   return (
     <div>
       <button>{tmp1}</button>
+      <button>{tmp2}</button>
+      <button>{tmp3}</button>
     </div>
   );
+}
+
+type Sub1Props = {};
+
+export default function Sub1({}: Sub1Props) {
+  return <div>Sub1</div>;
 }
