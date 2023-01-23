9bdf7fb787d21ed9541e102101959771908bdd84
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-23 13:58:49 +0700

    1234

--- frontend/src/components/pages/LoginPage/LoginPage.tsx
+++ frontend/src/components/pages/LoginPage/LoginPage.tsx
@@ -5,7 +5,14 @@ type LoginPageProps = {
 };
 
 const LoginPage: React.FC<any> = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div>
+      LoginPage
+      <h1 className="text-3xl font-bold underline bg-red-500 text-white w-[200px] rounded-3xl p-5">
+        Hello world!
+      </h1>
+    </div>
+  );
 };
 
 export default LoginPage;
