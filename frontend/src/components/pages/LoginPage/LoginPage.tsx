c72d2c08cbd05218156ebea84ebb8d516e49db2d
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-23 14:51:52 +0700

    1234

--- frontend/src/components/pages/LoginPage/LoginPage.tsx
+++ frontend/src/components/pages/LoginPage/LoginPage.tsx
@@ -5,11 +5,27 @@ type LoginPageProps = {
 };
 
 const LoginPage: React.FC<any> = () => {
+  let user = { username: "", password: "" };
+
   return (
     <div>
-      <form>
-        <input type="text" placeholder="Username" /> <br />
-        <input type="text" placeholder="Password" /> <br />
+      <form onSubmit={() => alert(JSON.stringify(user))}>
+        <input
+          onChange={(event) => {
+            user.username = event.target.value;
+          }}
+          type="text"
+          placeholder="Username"
+        />{" "}
+        <br />
+        <input
+          type="text"
+          placeholder="Password"
+          onChange={(event) => {
+            user.password = event.target.value;
+          }}
+        />{" "}
+        <br />
         <button type="submit">Submit</button> |
         <button type="button">Clear</button>
       </form>
