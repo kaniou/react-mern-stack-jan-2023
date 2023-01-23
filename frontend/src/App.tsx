69cb6bfdb151dbfffe3fd68d3d9c38c91666998d
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-23 13:36:10 +0700

    1234

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Page1 from "./components/Page1";
 import Page2 from "./components/Page2";
+import LoginPage from "./components/pages/LoginPage";
 
 type Props = {};
 
@@ -11,6 +12,7 @@ export default function App({}: Props) {
     <div>
       <Routes>
         <Route path="" element={<Navigate to="/page1" />} />
+        <Route path="login" element={<LoginPage />} />
         <Route path="page1" element={<Page1 />} />
         <Route path="page2" element={<Page2 />} />
         <Route path="*" element={<Navigate to="/page2" />} />
