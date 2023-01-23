735bce2a08fd4da9abe79022c897cac0abacf6f4
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-23 13:13:11 +0700

    1234

---
+++ frontend/src/components/Page1.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export default function Page1({}: Props) {
+  return <div>Page1</div>;
+}
---
+++ frontend/src/components/Page2.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export default function Page2({}: Props) {
+  return <div>Page2</div>;
+}
