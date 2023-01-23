e5a64d5139cff77a46ae5fa75aad823202c954db
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-23 11:51:25 +0700

    1234

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@mui/material";
 import React from "react";
 
 type Props = {};
 
 export default function App({}: Props) {
-  return <div>App</div>;
+  return (
+    <div>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </div>
+  );
 }
