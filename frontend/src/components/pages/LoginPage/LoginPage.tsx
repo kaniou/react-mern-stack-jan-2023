7ae6daaf18debe1351c82ecac9f7ba4341073a16
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-23 14:19:40 +0700

    1234

--- frontend/src/components/pages/LoginPage/LoginPage.tsx
+++ frontend/src/components/pages/LoginPage/LoginPage.tsx
@@ -8,6 +8,7 @@ const LoginPage: React.FC<any> = () => {
   return (
     <div>
       LoginPage
+      <img src="src/assets/react.svg" sizes="100" />
       <h1 className="text-3xl font-bold underline bg-red-500 text-white w-[200px] rounded-3xl p-5">
         Hello world!
       </h1>
