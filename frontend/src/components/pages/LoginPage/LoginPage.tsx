a89a7b9a832449bcc988534f798b91dd42832012
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-23 16:01:25 +0700

    1234

--- frontend/src/components/pages/LoginPage/LoginPage.tsx
+++ frontend/src/components/pages/LoginPage/LoginPage.tsx
@@ -11,7 +11,7 @@ const LoginPage: React.FC<any> = () => {
 
   return (
     <div>
-      <span style={{ backgroundColor: "#F00" }}>1234</span>
+      <span style={{ backgroundColor: "#F00", paddingTop: 100 }}>1234</span>
       <form
         onSubmit={(event) => {
           event.preventDefault();
