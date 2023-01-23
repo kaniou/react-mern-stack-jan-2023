b14a21e235c39c9918fa2fba159a0180d03c08c1
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-23 11:05:10 +0700

    1234

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,12 +14,17 @@ export default function App({}: Props) {
       <button>{tmp1}</button>
       <button>{tmp2}</button>
       <button>{tmp3}</button>
+      <br />
+      <Sub1 />
+      <Sub2 />
     </div>
   );
 }
 
+const Sub2 = () => <h1>Sub2</h1>;
+
 type Sub1Props = {};
 
-export default function Sub1({}: Sub1Props) {
+export function Sub1({}: Sub1Props) {
   return <div>Sub1</div>;
 }
