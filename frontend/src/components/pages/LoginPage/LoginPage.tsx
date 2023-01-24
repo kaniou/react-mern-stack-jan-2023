60db1840ccb8374555fc87805b019cce13c2bc57
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-24 10:24:59 +0700

    1234

--- frontend/src/components/pages/LoginPage/LoginPage.tsx
+++ frontend/src/components/pages/LoginPage/LoginPage.tsx
@@ -1,4 +1,12 @@
-import { Box, Button, Card, CardContent, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  InputAdornment,
+  TextField,
+  Typography,
+} from "@mui/material";
 // import Payment from "../../fragments/Payment/Payment";
 import Payment from "@/components/fragments/Payment";
 import React from "react";
@@ -13,6 +21,29 @@ const classes: any = {
 type Props = {};
 
 export default function LoginPage({}: Props) {
+  const showForm = () => {
+    return (
+      <form>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          label="Username"
+          autoComplete="email"
+          autoFocus
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          label="Password"
+          autoComplete="email"
+          autoFocus
+        />
+      </form>
+    );
+  };
+
   return (
     <Box sx={classes.root}>
       <Card sx={{ maxWidth: 345 }}>
