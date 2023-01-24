1838b31ce87252462b171329baf9c1ca6cf9a1e1
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-24 11:58:12 +0700

    1234

--- backend/tmp.ts
+++
@@ -1,9 +0,0 @@
-// node tmp.js
-// npm i -g nodemon
-console.log("I am a javascript file 1234");
-
-const tsTmp0 = 0;
-const tsTmp1 = true;
-const tsTmp2 = "lek";
-let tsTmp4 = 0;
-// tsTmp4 = false;
---
+++ backend/tmpTypescript.ts
@@ -0,0 +1,9 @@
+// node tmp.js
+// npm i -g nodemon
+console.log("I am a javascript file 1234");
+
+const tsTmp0: number = 0;
+const tsTmp1: boolean = true;
+const tsTmp2: string = "lek";
+let tsTmp4: number = 0;
+// tsTmp4 = false;
