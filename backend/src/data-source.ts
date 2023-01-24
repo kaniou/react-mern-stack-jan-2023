b0d1948cf2ee7b60d970c711e33984b5463b7161
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-24 15:44:04 +0700

    1234

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,16 +1,18 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { Product } from "./entity/Product";
-import { User } from "./entity/User";
+import { Counters } from "./entity/Counters";
+import { Products } from "./entity/Products";
+import { Transactions } from "./entity/Transactions";
+import { Users } from "./entity/Users";
 
 export const AppDataSource = new DataSource({
   host: process.env.MONGO_HOST ? process.env.MONGO_HOST : "localhost",
-  port: process.env.MONGO_PORT ? Number(process.env.MONGO_PORT) : 27019,
+  port: process.env.MONGO_PORT ? Number(process.env.MONGO_PORT) : 27018,
   type: "mongodb",
-  database: "test",
+  database: "demopos",
   synchronize: true,
   logging: false,
-  entities: [User, Product],
+  entities: [Counters, Users, Products, Transactions],
   migrations: [],
   subscribers: [],
 });
--- readme/readme_mongo_docker_standalone.md
+++ readme/readme_mongo_docker_standalone.md
@@ -12,7 +12,7 @@ docker run --name mongo-dev \
 ```
 docker run --name mongo-dev ^
 -v "%cd%/mongo-dev/db":/data/db ^
--v "%cd%/data/dummy_db_cmpos":/script ^
+-v "%cd%/dummy_db_cmpos":/script ^
 -p 27018:27017 -d --rm mongo
 ```
 
