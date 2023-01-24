5b7e34d74342de03593b2a588e8edb35009d5378
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-24 10:18:12 +0700

    1234

--- frontend/src/components/pages/LoginPage/LoginPage.tsx
+++ frontend/src/components/pages/LoginPage/LoginPage.tsx
@@ -1,19 +1,41 @@
-import { Box, Button } from "@mui/material";
+import { Box, Button, Card, CardContent, Typography } from "@mui/material";
 // import Payment from "../../fragments/Payment/Payment";
 import Payment from "@/components/fragments/Payment";
 import React from "react";
+import loginBg from "@/assets/images/bg4.jpg";
+
+const classes: any = {
+  root: { display: "flex", justifyContent: "center", alignItems: "center" },
+  submitBtn: { marginTop: 4 },
+  canelBtn: { marginTop: 2 },
+};
 
 type Props = {};
 
 export default function LoginPage({}: Props) {
   return (
-    <Box>
-      <Payment />
-      <Button sx={{ marginRight: 2 }} variant="contained">
-        Btn1
-      </Button>
-      <Button variant="contained">Btn2</Button>
-      <Button variant="contained">Btn3</Button>
+    <Box sx={classes.root}>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardContent>
+          <Typography gutterBottom variant="h5">
+            Login
+          </Typography>
+
+          {showForm()}
+        </CardContent>
+      </Card>
+      <style>
+        {`
+          body {
+            min-height: 100vh;
+            position: relative;
+            margin: 0;
+            background-size: cover;
+            background-image: url(${loginBg});
+            text-align: center;
+          }
+        `}
+      </style>
     </Box>
   );
 }
