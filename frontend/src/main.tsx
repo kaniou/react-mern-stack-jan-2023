a1a6c366e99cea20f6a6dbf89ec006784f43d91f
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-25 11:42:34 +0700

    1234

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 // HOC (Higher order component)
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
