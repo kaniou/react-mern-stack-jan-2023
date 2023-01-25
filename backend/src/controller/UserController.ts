c2d83b411a4ed9b1ae8229fe2b7d61e91689631f
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-25 09:49:16 +0700

    1234

--- backend/src/controller/UserController.ts
+++ backend/src/controller/UserController.ts
@@ -2,6 +2,7 @@ import { AppDataSource } from "../data-source";
 import { NextFunction, Request, Response } from "express";
 import { Users } from "../entity/Users";
 import { savedValue } from "../utils/cm-util";
+import * as bcrypt from "bcryptjs";
 
 export class UserController {
   private userRepository = AppDataSource.getMongoRepository(Users);
@@ -11,6 +12,8 @@ export class UserController {
       req.body.created = savedValue(req.body.created, new Date());
       req.body.level = savedValue(req.body.level, "normal");
       req.body.__v = savedValue(req.body.__v, 0);
+      req.body.password = await bcrypt.hash(req.body.password, 8);
+
       const doc = await this.userRepository.save(req.body);
       return { result: "ok", message: doc };
     } catch (e) {
