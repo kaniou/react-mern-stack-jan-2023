bbefcc5bbf3ab1d543858f9fcae3125fd44c944e
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-01-25 13:42:17 +0700

    1234

--- frontend/src/store/slices/counterSlice.ts
+++ frontend/src/store/slices/counterSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 
 type stateProp = {
@@ -6,17 +6,31 @@ type stateProp = {
 };
 const defaultState: stateProp = { count: 0 };
 
+const addAsync = createAsyncThunk("counter/addAsync", async () => {
+  // do something such as connecting server to feed something
+  // simulate such task with settimeout
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+});
+
+const removeAsync = createAsyncThunk("counter/removeAsync", async () => {
+  // do something such as connecting server to feed something
+  // simulate such task with settimeout
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+});
+
 const counterSlice = createSlice({
   name: "counter",
   initialState: defaultState,
   reducers: {
     add: (state) => {
+      // await new Promise((resolve) => setTimeout(resolve, 1000));
       state.count = state.count + 1;
     },
     remove: (state) => {
       state.count = state.count - 1;
     },
   },
+  extraReducers: (builder) => {},
 });
 
 export const { add, remove } = counterSlice.actions;
